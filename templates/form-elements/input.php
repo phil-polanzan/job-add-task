00c3d76cd270f4253f3d76801064c38c5039c22a
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-19 19:23:24 +0000

    #front-end: templates notes added

--- templates/form-elements/input.php
+++ templates/form-elements/input.php
@@ -5,4 +5,9 @@ require 'attributes.php';
 require 'label.php';
 ?>
 <input <?php echo $elementAttributes; ?>/>
+<?php if ($obj->notes): ?>
+	<div class="invalid-feedback"><?php echo $obj->notes; ?></div>
+<?php
+endif;
+
 
--- templates/form-elements/textarea.php
+++ templates/form-elements/textarea.php
@@ -5,4 +5,7 @@ require 'attributes.php';
 require 'label.php';
 ?>
 <textarea <?php echo $elementAttributes; ?>></textarea>
-
+<?php if ($obj->notes): ?>
+	<div class="invalid-feedback"><?php echo $obj->notes; ?></div>
+<?php
+endif;
