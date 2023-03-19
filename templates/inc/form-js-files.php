8b081168b106f9404036ae79b7e988d1e534a2d8
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-19 12:03:48 +0000

    #front-end: lib Js, datepicker settings imported as module

--- templates/inc/form-js-files.php
+++ templates/inc/form-js-files.php
@@ -2,4 +2,4 @@
 <script src="<?php echo ROOT_URL; ?>/lib/js/vendor/popper/popper.min.js"></script>
 <script src="<?php echo ROOT_URL; ?>/lib/js/vendor/bootstrap/bootstrap.min.js"></script>
 <script src="<?php echo ROOT_URL; ?>/lib/js/vendor/bootstrap/bootstrap-datepicker.min.js"></script>
-<script src="<?php echo ROOT_URL; ?>/lib/js/app/form.js"></script>
+<script type="module" src="<?php echo ROOT_URL; ?>/lib/js/app/form.js"></script>
