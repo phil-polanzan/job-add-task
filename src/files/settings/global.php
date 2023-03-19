e4ff04670b6c74d13a89bb302652bdf084291717
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-19 11:32:05 +0000

    #settings: global settings fix on setting the $rootPath value

--- src/files/settings/global.php
+++ src/files/settings/global.php
@@ -3,7 +3,7 @@
  * In this file global constants are defined
  */
 
-$rootPath = dirname(__DIR__, 2);
+$rootPath = dirname(__DIR__, 3);
 define('ROOT_PATH', $rootPath);
 
 $rootPathUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://{$_SERVER['HTTP_HOST']}";
