e80fe7de8a4eb911a51b71f444f0a3474ab754e0
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-19 15:21:38 +0000

    #settings: requests ajax post, content change for testing purpose

--- src/files/requests/ajax-post.php
+++ src/files/requests/ajax-post.php
@@ -1,4 +1,13 @@
 <?php
 require dirname(__DIR__, 3) . '/bootstrap-app.php';
 
+@header('Content-Type: text/html; charset=utf-8');
+@header('X-Robots-Tag: noindex');
+
 // TODO add logic for posting values submitted by ajax forms
+$data = [
+	'status' => 'ok',
+	'message' => 'Values stored'
+];
+echo json_encode($data);
+die();
