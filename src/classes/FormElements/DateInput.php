7916a177f2ae1ee1c8b427342b3a34149e029339
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-19 20:41:40 +0000

    #front-end: Form Elements DateInput class, construct bug fix

--- src/classes/FormElements/DateInput.php
+++ src/classes/FormElements/DateInput.php
@@ -4,9 +4,9 @@ namespace App\FormElements;
 
 class DateInput extends Input
 {
-	public function __construct(string $name, string $type, ?string $label, $value = null)
+	public function __construct(string $name, ?string $label, $value = null)
 	{
-		parent::__construct($name, 'text', $label);
+		parent::__construct($name, 'text', $label, $value);
 		$this->addAttributeKeys(['data-date-format']);
 		$this->setTemplateFile('input.php');
 	}
