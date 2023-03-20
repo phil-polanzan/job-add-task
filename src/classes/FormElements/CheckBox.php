ad918fc2cad2d622f4f0cec510dc7a83c1216f10
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-20 05:33:53 +0000

    #front-end: Form Elements CheckBoxClass added

---
+++ src/classes/FormElements/CheckBox.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\FormElements;
+
+class CheckBox extends Input
+{
+	public function __construct(string $name, ?string $label, $value = null)
+	{
+		parent::__construct($name, 'checkbox', $label, $value);
+		$this->setTemplateFile('input.php');
+	}
+}
