3b96a90bca93dc45e63a272d8c96bd084c3a3ea9
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-20 00:13:47 +0000

    #front-end: templates Label snippet, bug fix on setting required class

--- templates/form-elements/label.php
+++ templates/form-elements/label.php
@@ -1,5 +1,5 @@
 <?php
 $class = $obj->class;
-$class .= $obj->required ? ' required' : '';
+$class .= $obj->required ? 'required' : '';
 ?>
 <label for="<?php echo $obj->name; ?>" class="form-label <?php echo $class; ?>"><?php echo $obj->label; ?></label>
