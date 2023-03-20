ac757bd3b39998ab22c6db21e237d7d736c1fbd6
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-20 04:07:43 +0000

    #front-end: Response added message to form after submit

--- templates/inc/form-success-alert.php
+++ templates/inc/form-success-alert.php
@@ -1,6 +1,7 @@
 <div id="post-success" class="row alert alert-success div-hidden" role="alert">
 	<h4>Job added</h4>
 	<br/>
+	<p class="resp-msg"></p>
 	<p>
 		<a id="reload-form" href="<?php echo $_SERVER['REQUEST_URI']; ?>">Reload Form</a>
 	</p>
