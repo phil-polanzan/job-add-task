81230221f0fea1d376228a315c62715ba8373e7c
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-20 01:41:24 +0000

    #models: Property base class added

---
+++ src/classes/Exceptions/ModelPropertyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\ModelProperties\Property;
+
+class ModelPropertyException extends AppException
+{
+	public function __construct(Property $property, string $message) {
+		parent::__construct(get_class($property) . " {$property->getName()}: $message", 'ModelProperty');
+	}
+}
---
+++ src/classes/ModelProperties/Property.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\ModelProperties;
+
+use App\Exceptions\ModelPropertyException;
+
+abstract class Property
+{
+	private string $name;
+	private bool $emptyValueAllowed;
+	private $value;
+
+	public function __construct(string $name, bool $emptyValueAllowed = true)
+	{
+		$this->name = $name;
+		$this->emptyValueAllowed = $emptyValueAllowed;
+	}
+
+	public abstract function sanitise() : void;
+
+	public function getName() : string
+	{
+		return $this->name;
+	}
+
+	public function setValue($value) : void
+	{
+		// if the value is null it will be set to empty string
+		$this->value = $value ?? '';
+
+		if (!is_scalar($this->value)) {
+			$this->throwException('Value type not valid');
+		}
+	}
+
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	public function isEmptyValueAllowed() : bool
+	{
+		return $this->emptyValueAllowed;
+	}
+
+	public function validate() : bool
+	{
+		return !(!$this->isEmptyValueAllowed() && empty($this->value));
+	}
+
+	/**
+	 * @throws ModelPropertyException
+	 */
+	protected function throwException(string $message = 'Not valid value') : void
+	{
+		throw new ModelPropertyException($this, $message);
+	}
+}
