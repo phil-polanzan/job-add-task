35e187597c757acb0d096e8058ca15c17f7cdcff
phil-polanzan <filippo.polanzan@gmail.com>
2023-03-22 00:40:11 +0000

    #settings: Gloabl gloabl constant refactoring, the older version was giving problem to php storm to highlight them

--- src/files/settings/global.php
+++ src/files/settings/global.php
@@ -3,16 +3,28 @@
  * In this file global constants are defined
  */
 
-$notIsCli = php_sapi_name() !== 'cli';
-
-$constants = [
-	'ROOT_PATH' => dirname(__DIR__, 3),
-	'IS_CLI' => !$notIsCli,
-	'ROOT_URL' => $notIsCli ? (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://{$_SERVER['HTTP_HOST']}" : ''
-];
-
-foreach ($constants as $key => $value) {
-	if (!defined($key)) {
-		define($key, $value);
-	}
+if (!defined('ROOT_PATH')) {
+	define('ROOT_PATH', dirname(__DIR__, 3));
 }
+
+if (!defined('IS_CLI')) {
+	define('IS_CLI', php_sapi_name() === 'cli');
+}
+
+if (!defined('ROOT_URL') && !IS_CLI) {
+	define('ROOT_URL', (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://{$_SERVER['HTTP_HOST']}");
+}
+//
+//$notIsCli = php_sapi_name() !== 'cli';
+//
+//$constants = [
+//	'ROOT_PATH' => dirname(__DIR__, 3),
+//	'IS_CLI' => !$notIsCli,
+//	'ROOT_URL' => $notIsCli ?  : ''
+//];
+//
+//foreach ($constants as $key => $value) {
+//	if (!defined($key)) {
+//		define($key, $value);
+//	}
+//}
