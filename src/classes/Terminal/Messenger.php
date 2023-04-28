b7caeb54d4dc12429d322f3d7b65368f5d4325d1
phil-polanzan <filippo.polanzan@gmail.com>
2023-04-28 23:52:07 +0100

    #dev: Terminal Class refactoring, constants removed, jst used in the class

--- src/classes/Terminal/Messenger.php
+++ src/classes/Terminal/Messenger.php
@@ -4,40 +4,28 @@ namespace App\Terminal;
 
 class Messenger
 {
-	const WARNING_TYPE = 'warning';
-	const SUCCESS_TYPE = 'success';
-	const ERROR_TYPE = 'error';
-	const INFO_TYPE = 'info';
-
-	private static array $colours = [
-		self::WARNING_TYPE => '1;33',
-		self::SUCCESS_TYPE => '0;32',
-		self::ERROR_TYPE => '0;31',
-		self::INFO_TYPE => '1;37'
-	];
-
-	public static function getMsg(string $msg, string $type) : string
+	public static function getMsg(string $msg, string $type, string $colour) : string
 	{
-		return sprintf("\033[%sm[%s] %s\033[0m" . PHP_EOL,  self::$colours[$type], $type, $msg);
+		return sprintf("\033[%sm[%s] %s\033[0m" . PHP_EOL,  $colour, $type, $msg);
 	}
 
 	public static function error(string $msg)
 	{
-		echo self::getMsg($msg, self::ERROR_TYPE);
+		echo self::getMsg($msg, 'Error', '0;31');
 	}
 
 	public static function success(string $msg)
 	{
-		echo self::getMsg($msg, self::SUCCESS_TYPE);
+		echo self::getMsg($msg, 'Success', '0;32');
 	}
 
 	public static function info(string $msg)
 	{
-		echo self::getMsg($msg, self::INFO_TYPE);
+		echo self::getMsg($msg, 'Info', '1;37');
 	}
 
 	public static function warning(string $msg)
 	{
-		echo self::getMsg($msg, self::WARNING_TYPE);
+		echo self::getMsg($msg, 'Warning', '1;33');
 	}
 }
