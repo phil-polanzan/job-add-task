bac53251b62743938b02e5d1584c975bc3eb7319
phil-polanzan <filippo.polanzan@gmail.com>
2023-04-28 23:44:02 +0100

    #dev: Index file small fix

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ extract([
 	'objArgs' => ['Add New Job', null]
 ]);
 
-$file = include ROOT_PATH . "/src/files/forms/$formType/$model/$formAction.php";
+$file = ROOT_PATH . "/src/files/forms/$formType/$model/$formAction.php";
 $file = file_exists($file) ? $file : ROOT_PATH . '/src/files/forms/entry.php';
 require $file;
