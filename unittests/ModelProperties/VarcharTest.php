424a7bfc692927fad68d8bde56f28252fa808727
phil-polanzan <filippo.polanzan@gmail.com>
2025-04-21 16:19:43 +0100

    #unittest: Varchar, test cases added

---
+++ unittests/ModelProperties/VarcharTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Unittests\ModelProperties;
+
+use App\Exceptions\AppException;
+use App\ModelProperties\Varchar;
+
+class VarcharTest extends ModelPropertiesTest
+{
+	public function testNegativeLength() : void {
+		try {
+			$varchar = new Varchar('test', -1);
+		} catch (AppException $e) {
+			$this->assertEqualsModelPropertyException($e);
+		}
+
+	}
+
+	public function testOutOfLength() : void {
+		try {
+			$varchar = new Varchar('test', 256);
+		} catch (AppException $e) {
+			$this->assertEqualsModelPropertyException($e);
+		}
+	}
+
+	public function testSuccess() : void {
+		$varchar = new Varchar('test', 50);
+		$this->assertInstanceOf(Varchar::class, $varchar);
+	}
+}
