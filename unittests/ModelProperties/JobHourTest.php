456c5e497fca8777dcdfcf4dee96aaad18ebebd3
phil-polanzan <filippo.polanzan@gmail.com>
2025-04-21 16:01:13 +0100

    #unittest: JobHours, JobHourTest jobHour property sreplace with local varaible

--- unittests/ModelProperties/JobHourTest.php
+++ unittests/ModelProperties/JobHourTest.php
@@ -7,18 +7,16 @@ use App\ModelProperties\JobHour;
 
 class JobHourTest extends ModelPropertiesTest
 {
-	private JobHour $jobHour;
-
 	public function testNotValidStep(): void {
 		try {
-			$this->jobHour = new JobHour('test', 0.3);
+			$jobHour = new JobHour('test', 0.3);
 		} catch (AppException $e) {
 			$this->assertEqualsModelPropertyException($e);
 		}
 	}
 
 	public function testSuccess(): void {
-		$this->jobHour = new JobHour('test', JobHour::UNIT_HALF_HOUR);
-		$this->assertInstanceOf(JobHour::class, $this->jobHour);
+		$jobHour = new JobHour('test', JobHour::UNIT_HALF_HOUR);
+		$this->assertInstanceOf(JobHour::class, $jobHour);
 	}
 }
